1f299a8761ae11974aaf6910c59175da6784f1da
Оксана Александрова <aleksandrovao767@gmail.com>
2022-01-22 18:47:47 +0200

    first commit

---
+++ burger.js
@@ -0,0 +1,21 @@
+(function(){
+  const burger = document?.querySelector('[data-burger]');
+  const menu = document?.querySelector('[data-menu]');
+
+  burger?.addEventListener('click', (e) => {
+    burger?.classList.toggle('burger--active');
+    menu?.classList.toggle('menu--active');
+  });
+})();
+
+
+
+let burger = document.querySelector('.burger');
+burger.addEventListener('click', function(){
+  let menu = document.querySelector('.nav');
+  let menuOpen = document.querySelector('nav--active');
+  let body = document.querySelector('body');
+  let lock = document.querySelector('lock');
+  menu.classList.toggle('nav--active');
+  body.classList.toggle('lock');
+})
